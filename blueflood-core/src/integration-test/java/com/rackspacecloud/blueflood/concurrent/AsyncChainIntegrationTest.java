ae2d919578570976d939434894dd308c5b9eca28
Chinmay Gupte <chinmay.gupte@rackspace.com>
2014-05-12 16:44:45 -0700

    add a comment

--- blueflood-core/src/integration-test/java/com/rackspacecloud/blueflood/concurrent/AsyncChainIntegrationTest.java
+++ blueflood-core/src/integration-test/java/com/rackspacecloud/blueflood/concurrent/AsyncChainIntegrationTest.java
@@ -40,6 +40,7 @@ public class AsyncChainIntegrationTest extends IntegrationTestBase {
     public void testAsyncChainWithNoOp() throws Exception {
         List<Metric> metrics = makeRandomIntMetrics(10000);
         ListenableFuture<Boolean> returnedFuture = asyncChain.apply(metrics);
+        // get should not block if its async
         returnedFuture.get();
         // processing of metrics should still not be complete because its async
         Assert.assertFalse(processingComplete);
